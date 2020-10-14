8a3f3e8afad302b0486a949c44207b893467e8e1
LIssette Quebrada <lissque604@gmail.com>
2020-10-14 01:15:53 -0500

    CREACION VENTANA COMPILADOR
    Creacion ventana compilador con area de texto + tabla. Todo funciona correctamente. Falta terminar de configurar la tabla

--- src/co/edu/uniquindio/compilador/app/Aplicacion.kt
+++ src/co/edu/uniquindio/compilador/app/Aplicacion.kt
@@ -1,5 +1,26 @@
 package co.edu.uniquindio.compilador.app
+import javafx.application.Application
+import javafx.fxml.FXMLLoader
+import javafx.scene.Parent
+import javafx.scene.Scene
+import javafx.stage.Stage
 
-class Aplicacion {
-    
+class Aplicacion : Application() {
+
+    override fun start(primaryStage: Stage?) {
+        val loader = FXMLLoader(Aplicacion :: class.java.getResource("/Inicio.fxml"))
+        val parent : Parent = loader.load()
+        val scene = Scene(parent)
+        primaryStage?.scene = scene
+        primaryStage?.title = "Compilador"
+        primaryStage?.show()
+    }
+
+    companion object{
+        @JvmStatic
+        fun main (args: Array<String>){
+            launch(Aplicacion::class.java)
+        }
+    }
+
 }
--- src/co/edu/uniquindio/compilador/controladores/InicioController.kt
+++ src/co/edu/uniquindio/compilador/controladores/InicioController.kt
@@ -1,5 +1,22 @@
 package co.edu.uniquindio.compilador.controladores
 
+import co.edu.uniquindio.compilador.lexico.AnalizadorLexico
+import javafx.event.ActionEvent
+import javafx.fxml.FXML
+import javafx.scene.control.TextArea
+
 class InicioController {
+
+    @FXML
+    lateinit var codigoFuente : TextArea
+
+    @FXML
+    fun analizarCodigo(e : ActionEvent){
+        if (codigoFuente.length>0){
+            val lexico = AnalizadorLexico(codigoFuente.text)
+            lexico.analizar()
+            print(lexico.listaTokens)
+        }
+    }
 
 }
