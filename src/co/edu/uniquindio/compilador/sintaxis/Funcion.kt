1748bc90dc7829dfd6f25f28f55aabd88bcf808a
LIssette Quebrada <lissque604@gmail.com>
2020-11-20 02:14:29 -0500

    CORRECCION
    correcion en lista parametros

--- src/co/edu/uniquindio/compilador/sintaxis/Funcion.kt
+++ src/co/edu/uniquindio/compilador/sintaxis/Funcion.kt
@@ -3,7 +3,7 @@ package co.edu.uniquindio.compilador.sintaxis
 import co.edu.uniquindio.compilador.lexico.Token
 import javafx.scene.control.TreeItem
 
-class Funcion(var nombreFuncion:Token, var tipoRetorno:Token, var listaParametros: ArrayList<Parametro>, var listaSentencias: ArrayList<Sentencia>) {
+class Funcion(var nombreFuncion:Token, var tipoRetorno:Token, var listaParametros: ArrayList<Parametro>?, var listaSentencias: ArrayList<Sentencia>) {
 
     override fun toString(): String {
         return "Funcion(nombreFuncion=$nombreFuncion, tipoRetorno=$tipoRetorno, listaParametros=$listaParametros, listaSentencias=$listaSentencias)"
@@ -15,8 +15,10 @@ class Funcion(var nombreFuncion:Token, var tipoRetorno:Token, var listaParametro
         raiz.children.add(TreeItem("Nombre: ${nombreFuncion.lexema}"))
         raiz.children.add(TreeItem("Tipo de Retorno: ${tipoRetorno.lexema}"))
         var raizParametros = TreeItem("Parametros")
-        for ( parametro in listaParametros){
-            raizParametros.children.add(parametro.getArbolVisual())
+        if (listaParametros!=null){
+            for ( parametro in listaParametros!!){
+                raizParametros.children.add(parametro.getArbolVisual())
+            }
         }
         raiz.children.add(raizParametros)
         var raizSentencia = TreeItem("Sentencias")
